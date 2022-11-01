014e4b0f1e6271df42eb6ecf8b0df4cde9bec80a
Tomas <tomastokar@gmail.com>
2022-11-01 12:59:11 -0600

    list and 2d array representation

---
+++ src/classes.py
@@ -0,0 +1,69 @@
+import itertools
+import numpy as np
+
+class CPDTable:
+    def __init__(self, variable, values, evidence = []):
+        self.variable = variable
+        self.evidence = evidence
+        self.values = np.array(values)
+        self.legend = self._get_legend()
+        
+    def _get_legend(self):
+        values = (True, False)
+        legend = itertools.product(*(values for _ in self.evidence))
+        legend = np.array(legend)
+        return legend
+    
+    def _normalize(self):
+        self.values /= self.values.sum(axis = 0)
+    
+    def marginalize(self, variable):        
+        i = self.evidence.index(variable)
+        l = self.legend[:,i]
+        values = self.values[:, l] + self.values[:,~l]
+        evidence = [e for e in self.evidence if e != variable]
+        cpd_table = CPDTable(self.variable, values, evidence)
+        return cpd_table
+
+    def reduce(self, variable, value):             
+        i = self.evidence.index(variable)
+        l = self.legend[:,i]
+        idx = np.where(l == value)[0]
+        values = self.values[:,idx]
+        evidence = [e for e in self.evidence if e != variable]
+        cpd_table = CPDTable(self.variable, values, evidence)
+        return cpd_table
+    
+        
+class BNet:
+    def __init__(self, edges):
+        self.edges = edges
+        self.nodes = set([node for edge in edges for node in edge])
+        self.in_degree, self.out_degree = self._get_degree()
+        self.cpd_tables = []
+        
+    def _get_degree(self):
+        in_degree = {n : 0 for n in self.nodes}
+        out_degree = {n : 0 for n in self.nodes}
+        for edge in self.edges:
+            in_degree[edge[0]] += 1
+            out_degree[edge[1]] += 1
+        return in_degree, out_degree
+    
+    def add_cpd(self, cpd_table):
+        self.cpd_tables.append(cpd_table)
+        
+    def elminate_variable(self, variable):
+        cpds = []
+        for cpd in self.cpd_tables:
+            if variable in cpd.evidence:
+                
+    
+    def eval_query(self, variable, evidence = []):
+        keep_nodes = set(variable).union(evidence)
+        elim_nodes = [node for node in self.nodes if node not in keep_nodes]
+        
+        if len(elim_nodes) > 0:
+            elimination_order = sorted(elim_nodes, key=self.in_degee.get)
+        
+        
